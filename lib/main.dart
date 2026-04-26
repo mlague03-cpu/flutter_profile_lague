b74c22bfb08a1653932cf4a151ec78ebb8a20b62
mlague03-cpu <mlague03@asscat.edu.ph>
2026-04-26 22:37:09 +0800

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -18,43 +18,61 @@ class ProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.purple[50],
+
       appBar: AppBar(
         title: Text("My Developer Profile"),
         centerTitle: true,
         backgroundColor: Colors.purple,
       ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
 
-            Icon(Icons.person, size: 80, color: Colors.purple),
+            Icon(Icons.person, size: 90, color: Colors.purple),
 
             SizedBox(height: 15),
 
             Text(
-              "MJ LAGUE", // CHANGE TO YOUR NAME
+              "MJ Lague",
               style: TextStyle(
-                fontSize: 24,
+                fontSize: 26,
                 fontWeight: FontWeight.bold,
+                color: Colors.purple[900],
               ),
             ),
 
             SizedBox(height: 10),
 
-            Text("BSIT-3B"), // CHANGE SECTION
+            Text(
+              "BSIT-3B",
+              style: TextStyle(fontSize: 18),
+            ),
 
             SizedBox(height: 10),
 
-            Text("Age: 21"), // CHANGE AGE
+            Text(
+              "Age: 21",
+              style: TextStyle(fontSize: 18),
+            ),
+
+            SizedBox(height: 15),
+
+            Text(
+              "Top 3 Hobbies",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            ),
 
             SizedBox(height: 10),
 
-            Text("Hobbies:"),
-            Text("• Coding"),
+            Text("• Cleaning"),
             Text("• Playing Games"),
             Text("• Watching Movies"),
-
           ],
         ),
       ),
